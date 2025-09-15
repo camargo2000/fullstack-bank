3e5395dbb51975c7b48f96fbe7cc14a6e167956d
sneak-dev <xfengefbpz9@gmx.com>
2025-09-15 13:15:57 +0300

    deleted comment

--- app/frontend/components/TransactionsList.tsx
+++ app/frontend/components/TransactionsList.tsx
@@ -39,7 +39,7 @@ export default function TransactionsList({
           break;
 
         case StatusCodes.UNAUTHORIZED:
-          setErrorMessage('Seu login expirou');
+          setErrorMessage('Seu login ');
           break;
 
         default:
